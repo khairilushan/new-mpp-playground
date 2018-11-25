7a34580999a95e49b9cf3185dc4a09b78562cd1e
Khairil Ushan <khairil.ushan@gmail.com>
2018-11-25 16:14:55 +0700

    Initial commit

---
+++ apps/iosApp/libs/libs.h
@@ -0,0 +1,18 @@
+//
+//  libs.h
+//  libs
+//
+//  Created by Khairil Ushan on 25/11/18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libs.
+FOUNDATION_EXPORT double libsVersionNumber;
+
+//! Project version string for libs.
+FOUNDATION_EXPORT const unsigned char libsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libs/PublicHeader.h>
+
+
